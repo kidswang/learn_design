e738dd34e70d9ccc42f3c807d25a5abaa66706c5
wangzhenglei <705016626@qq.com>
2020-11-18 14:31:26 +0800

    type: modified 修改

--- src/main/java/com/waiwaiwai/mydesign/openandclose/newblan/AlertApplicationContext.java
+++ src/main/java/com/waiwaiwai/mydesign/openandclose/newblan/AlertApplicationContext.java
@@ -17,6 +17,9 @@ public class AlertApplicationContext {
     private Notification notification;
     private NewAlert alert;
 
+
+
+
     public void initializeBeans() {
         alertRule = new AlertRule();
         notification = new Notification();
